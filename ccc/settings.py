f907749e459b6347afb1f5c7111a8d060a9e157e
De-miz <115442247+De-miz@users.noreply.github.com>
2022-11-04 03:53:19 +0000

    Update settings.py

--- ccc/settings.py
+++ ccc/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-_)^q4)du@!)xi-3g(2@b&3su52jx#xlu_0%b2x6(&&v8260*f2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['datasciencefirstdeployment.herokuapp.com']
 
 
 # Application definition
