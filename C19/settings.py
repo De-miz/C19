0b16c825600e699533ec4304c8ea512e3745685f
De-miz <115442247+De-miz@users.noreply.github.com>
2022-11-04 01:03:19 +0000

    Update settings.py

--- C19/settings.py
+++ C19/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_)^q4)du@!)xi-3g(2@b&3su52jx#xlu_0%b2x6(&&v8260*f2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['datasciencefirstdeployment.herokuapp.com']
 
 
 # Application definition
